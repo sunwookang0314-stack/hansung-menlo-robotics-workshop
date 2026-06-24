0d66555f0248790412af98df85628c521e0d486a
Jiwoon Ha <juneha1120@gmail.com>
2026-06-24 13:39:54 +0800

    Add shared starter file edit warnings

--- menlo_runner/programs/__init__.py
+++ menlo_runner/programs/__init__.py
@@ -1,3 +1,8 @@
-﻿"""Runnable Menlo robot programs."""
+# Starter code: do not edit this shared file for project submissions.
+# Put project code in a project notebook or a new file under menlo_runner/programs/.
+# 스타터 코드: 프로젝트 제출을 위해 이 공용 파일을 직접 수정하지 마세요.
+# 프로젝트 코드는 프로젝트 노트북 또는 menlo_runner/programs/의 새 파일에 작성하세요.
+
+"""Runnable Menlo robot programs."""
 
 
